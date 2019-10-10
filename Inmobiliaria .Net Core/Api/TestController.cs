7fed508485c558f587afe7936520835aaaf99064
Mariano Luzza <marianoluzza@gmail.com>
2019-10-09 23:20:56 -0300

    Api test

---
+++ Inmobiliaria .Net Core/Api/TestController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Inmobiliaria_.Net_Core.Api
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                return Ok(new
+                {
+                    Mensaje = "Éxito",
+                    Error = 0,
+                    Resultado = new
+                    {
+                        Clave = "Key",
+                        Valor = "Value"
+                    },
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
